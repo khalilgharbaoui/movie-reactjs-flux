b800a62deede132523748feb9e9ddaecba733d5f
khalilgharbaoui <kaygeee@gmail.com>
2016-04-16 06:19:10 +0200

    improved  gulp file and added live reload with gulp-connect also removed reactify and added babelify

---
+++ src/js/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import AppActions from '../actions/AppActions';
+import AppStore from '../stores/AppStore';
+
+
+ class App extends React.Component{
+  render(){
+    return(
+      <div></div>
+    )
+  }
+}
+
+
+
+// var React = require('react');
+// var AppActions = require('../actions/AppActions');
+// var AppStore = require('../stores/AppStore');
+//
+// var App = React.createClass({
+//   render :function(){
+//     returen
+//   }
+// })
+
+export default App;
